8163abd16650efa9ecb5392d9970728874e54670
Vare <dm.va@hotmail.com>
2025-12-03 20:08:16 -0300

    fix: fix my _root file that wasnt displaying the outlet correctly

--- src/routes/(shop)/route.tsx
+++ src/routes/(shop)/route.tsx
@@ -1,9 +1,21 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { ShopHeader } from "~/components/shop/shop-header";
+import { ShopFooter } from "~/components/shop/shop-footer";
+import { MiniCart } from "~/components/shop/mini-cart";
 
-export const Route = createFileRoute('/(shop)')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/(shop)")({
+  component: ShopLayout,
+});
 
-function RouteComponent() {
-  return <div>Hello "/(shop)"!</div>
+function ShopLayout() {
+  return (
+    <div className="flex min-h-svh flex-col">
+      <ShopHeader />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <ShopFooter />
+      <MiniCart />
+    </div>
+  );
 }
